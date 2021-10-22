d42156ba2727cccfc547d61c9872264f3774254a
zhanbo <zhanbo@yunexpress.cn>
2021-10-22 18:28:59 +0800

    upadte by zhanbo

--- app/src/main/java/com/lfkj/aidldatatranslatedemo/MainActivity.java
+++ app/src/main/java/com/lfkj/aidldatatranslatedemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
        //git 合并测试
         System.out.println("这是master分支，为合并分支测试");
+		System.out.println("这是master分支，为合并分支测试 zhanbo");
     }
 }
