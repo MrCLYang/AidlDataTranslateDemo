b7a49a7ddbaf7b59a59906488cdeec6e6d9eca02
yangchenglei <931032120@qq.com>
2021-10-22 17:48:32 +0800

    创建V1.01分支

--- app/src/main/java/com/lfkj/aidldatatranslatedemo/MainActivity.java
+++ app/src/main/java/com/lfkj/aidldatatranslatedemo/MainActivity.java
@@ -10,14 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
-
-
-
-
-
+       //git 合并测试
+        System.out.println("分支V1.01，为合并分支测试");
     }
 }
